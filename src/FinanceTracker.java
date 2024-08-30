cc2c5cd6795b709b5aa5ece98536eca7392cdd57
Dhruvin Patel <dhruvinvpatel12@gmail.com>
2024-08-30 08:28:14 -0400

    basic functionality

--- src/FinanceTracker.java
+++ src/FinanceTracker.java
@@ -1,3 +1,32 @@
-public class FinanceTracker {
+import java.util.ArrayList;
+import java.util.List;
 
+public class FinanceTracker {
+    private List<Transaction> transactions;
+
+    public FinanceTracker() {
+        transactions = new ArrayList<>();
+    }
+
+    public void addTransaction(String type, double amount, String description, String date) {
+        transactions.add(new Transaction(type, amount, description, date));
+    }
+
+    public double calculateBalance() {
+        double balance = 0;
+        for (Transaction t : transactions) {
+            if (t.getType().equalsIgnoreCase("Income")) {
+                balance += t.getAmount();
+            } else {
+                balance -= t.getAmount();
+            }
+        }
+        return balance;
+    }
+
+    public void viewTransactions() {
+        for (Transaction t : transactions) {
+            System.out.println(t);
+        }
+    }
 }
--- src/Transaction.java
+++ src/Transaction.java
@@ -1,3 +1,34 @@
 public class Transaction {
+    private String type; // "Income" or "Expense"
+    private double amount;
+    private String description;
+    private String date;
 
+    public Transaction(String type, double amount, String description, String date) {
+        this.type = type;
+        this.amount = amount;
+        this.description = description;
+        this.date = date;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    @Override
+    public String toString() {
+        return date + " - " + type + ": " + amount + " (" + description + ")";
+    }
 }
